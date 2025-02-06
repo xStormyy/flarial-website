eb80db910529bdefb6da31dd2bdd7cbd35329805
Ashank <ashank@ashank.tech>
2025-02-06 20:06:58 +0800

    Update React and React-DOM versions, add type definitions for luxon, and enhance API response handling

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 
+/* eslint-disable no-var */
+declare global {
+  var mongoose: {
+    conn: mongoose.Connection | null;
+    promise: Promise<mongoose.Connection> | null;
+  } | undefined;
+}
+/* eslint-enable no-var */
+
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
@@ -12,19 +21,19 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectDB() {
-  if (cached.conn) {
+async function connectDB(): Promise<mongoose.Connection> {
+  if (cached?.conn) {
     return cached.conn;
   }
 
-  if (!cached.promise) {
+  if (!cached?.promise) {
     const opts = {
       bufferCommands: false,
     };
 
     mongoose.set('strictQuery', true);
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
+      return mongoose.connection;
     });
   }
 
