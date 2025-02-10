663a017b474dd6a47af9e1bb8290acb98dc40929
TTF-fog <adh.ishaan10@gmail.com>
2025-02-10 18:40:45 +0530

    added initial anim

--- src/components/ui/ContributorCard.tsx
+++ src/components/ui/ContributorCard.tsx
@@ -9,25 +9,37 @@ interface ContributorCardProps {
 
 export function ContributorCard({ name, roles, github }: ContributorCardProps) {
   return (
-    <Card variant="dark" className="w-full">
-    <motion.div>
-      <div className="flex flex-col gap-2">
-        <motion.a className="text-lg font-semibold text-white hover:scale[2] " href={github} whileHover={{ scale: 1.02, color:"aqua"}}>{name}</motion.a>
 
-        <div className="flex flex-wrap gap-2">
-          {roles.map((role, index) => (
-            <span
-              key={index}
-              className="px-2 py-1 text-sm rounded-full bg-gray-800 text-gray-200"
-            >
+      <motion.div
+
+          initial={{opacity: 0, y: 20}}
+          animate={{opacity: 1, y: 0}}
+          transition={{duration: 1, delay: 0.4}}
+          className="flex flex-col sm:flex-row gap-4 justify-center"
+      >
+
+          <Card variant="dark" className="w-full">
+
+            <div className="flex flex-col gap-2">
+              <motion.a className="text-lg font-semibold text-white hover:scale[2] " href={github}
+                        whileHover={{scale: 1.02, color: "aqua"}}>{name}</motion.a>
+
+              <div className="flex flex-wrap gap-2">
+                {roles.map((role, index) => (
+                    <span
+                        key={index}
+                        className="px-2 py-1 text-sm rounded-full bg-gray-800 text-gray-200"
+                    >
               {role}
             </span>
 
-          ))}
+                ))}
 
-        </div>
-      </div>
-    </motion.div>
-    </Card>
-  );
+              </div>
+            </div>
+
+          </Card>
+      </motion.div>
+)
+;
 }
